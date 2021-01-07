aca035b59ac0d39bffe125586545a2d4f5109aae
Matija Osojnik <matija1.osojnik@gmail.com>
2021-01-07 19:34:48 +0100

    Update Form design, add confirmations controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_172300) do
+ActiveRecord::Schema.define(version: 2021_01_07_181538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 2021_01_07_172300) do
     t.string "icon_url"
     t.string "name"
     t.string "surname"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
