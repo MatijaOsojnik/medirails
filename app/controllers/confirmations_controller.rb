83933b11e79edc8f290cd3834091ea6a0580c054
Matija Osojnik <matija1.osojnik@gmail.com>
2021-01-07 20:47:13 +0100

    Delete Confirmations controller

--- app/controllers/confirmations_controller.rb
+++
@@ -1,7 +0,0 @@
-class ConfirmationsController < Devise:ConfirmationsController
-    private
-        def after_confirmation_path_for(resource_name, resource)
-            sign_in(resource)
-            articles_path
-        end
-end
