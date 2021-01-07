9f2676cd525b47a8c32767772b66294edfe39876
Matija Osojnik <matija1.osojnik@gmail.com>
2021-01-07 11:24:08 +0100

    Fix Config

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_083858) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "avatar_url"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
