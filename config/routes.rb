08e5c09e5070f9106bac6f0dc2d794b6ee9754d1
Matija Osojnik <matija1.osojnik@gmail.com>
2021-01-07 19:38:19 +0100

    Remove confirmations from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :articles 
-  devise_for :users, :controllers {
-    confirmations: 'confirmations'
-  }
+  devise_for :users
   root to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
