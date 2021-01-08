408ccc19b2b5b2b673f728fbb95075b18204c568
Matija Osojnik <matija1.osojnik@gmail.com>
2021-01-08 10:13:55 +0100

    Allow name, surname in params + update index design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       if user_signed_in?
         concat link_to "Uredi profil", edit_user_registration_path, class: 'btn btn-sm btn-outline-secondary'
         concat " "
-        concat link_to "Odjava", destroy_user_session_path, method: :get, class: 'btn btn-sm btn-outline-secondary mx-2'
+        concat link_to "Odjava", destroy_user_session_path, method: :delete, class: 'btn btn-sm btn-outline-secondary mx-2'
       else
         concat link_to "Prijava", new_user_session_path, class: 'btn btn-sm btn-outline-secondary'
         concat " "
