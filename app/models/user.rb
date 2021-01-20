81c3d7a8c4fc11549f81bb818bb8c7d125e2814c
Matija Osojnik <matija1.osojnik@gmail.com>
2021-01-20 10:25:49 +0100

    Add Google OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable,
-  :omniauthable, omniauth_providers: %i[facebook]
+  :omniauthable, omniauth_providers: %i[facebook google_oauth2]
   has_many :articles, dependent: :destroy
   has_many :bookmarks
 
   def self.new_with_session(params, session)
   super.tap do |user|
     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      user.email = data["email"] if user.email.blank?
+    end
+    if data = session["devise.google_oauth2"] && session["devise.google_oauth2_data"]["extra"]["raw_info"]
       user.email = data["email"] if user.email.blank?
     end
   end
@@ -20,6 +23,7 @@ def self.from_omniauth(auth)
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.name = auth.info.name   # assuming the user model has a name
+    user.icon_url = auth.info.image
   end
 end
 end
