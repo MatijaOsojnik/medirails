198f6ebcaed30b1582db7713297c2be3a04cb607
Matija Osojnik <matija1.osojnik@gmail.com>
2021-01-22 15:03:33 +0100

    Show User Articles, update User Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
     def show
         @user = User.find_by_id(params[:id])
+        @articles = Article.all.where(user: params[:id])
+    end
+    def index
+
     end
 end
